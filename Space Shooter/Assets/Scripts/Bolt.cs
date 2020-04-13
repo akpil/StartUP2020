bb418a4bebf3f5e70788c879a9e870fa66dbff74
Akpil <jyk0802@naver.com>
2020-04-13 16:49:13 +0900

    Space shooter Player setup, Player movement, Player fire

---
+++ Space Shooter/Assets/Scripts/Bolt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+    private Rigidbody mRB;
+    [SerializeField]
+    private float mSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRB = GetComponent<Rigidbody>();
+        mRB.velocity = transform.forward * mSpeed;
+    }
+
+}
---
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Fire bolt")]
+    [SerializeField]
+    private GameObject Bolt;
+    [SerializeField]
+    private Transform mBoltPos;
+    [SerializeField]
+    private float mFireLate;
+    private float mCurrentFireLate;
+
+    private Rigidbody mRB;
+    [Header("Movement")]
+    [SerializeField]
+    private float mSpeed;
+    [SerializeField]
+    private float mXMax, mXMin, mZMax, mZMin;
+    [SerializeField]
+    private float mTilted = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRB = GetComponent<Rigidbody>();
+        mCurrentFireLate = mFireLate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        direction = direction.normalized;
+
+        mRB.velocity = direction * mSpeed;
+        //transform.Translate(direction * Time.deltaTime);
+
+        mRB.rotation = Quaternion.Euler(0, 0, horizontal * - mTilted);
+
+        mRB.position = new Vector3(Mathf.Clamp(mRB.position.x, mXMin, mXMax), 
+                                   0,
+                                   Mathf.Clamp(mRB.position.z, mZMin, mZMax));
+
+        if(Input.GetButton("Fire1") && mCurrentFireLate >= mFireLate)
+        {
+            GameObject obj = Instantiate(Bolt);
+            obj.transform.position = mBoltPos.position;
+            mCurrentFireLate = 0;
+        }
+        else
+        {
+            mCurrentFireLate += Time.deltaTime;
+        }
+    }
+}
