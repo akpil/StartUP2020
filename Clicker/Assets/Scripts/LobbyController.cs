1cbfa762d6da4b93be6b250e34e6af2cc01e5385
Akpil <jyk0802@naver.com>
2020-05-06 15:08:22 +0900

    clicker title scene setup, touchmanager

---
+++ Clicker/Assets/Scripts/LobbyController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LobbyController : MonoBehaviour
+{
+    [SerializeField]
+    private Button mStartButton;
+    [SerializeField]
+    private Text mStartText;
+    [SerializeField]
+    private float mAlphaAnimPeriod = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mStartButton.onClick.AddListener(() => { SceneManager.LoadScene(1); });
+        mStartButton.interactable = false;
+    }
+
+    public void ActivateGameStart()
+    {
+        mStartButton.interactable = true;
+        StartCoroutine(AlphaAnim());
+    }
+
+    private IEnumerator AlphaAnim()
+    {
+        WaitForFixedUpdate fixedUpdate = new WaitForFixedUpdate();
+        bool bAscending = true;
+        float halfTime = mAlphaAnimPeriod / 2;
+        Color color = new Color(0, 0, 0, 1 / halfTime * Time.fixedDeltaTime);
+        while(true)
+        {
+            yield return fixedUpdate;
+            if(bAscending)
+            {
+                mStartText.color += color;
+                if(mStartText.color.a >= 1)
+                {
+                    bAscending = false;
+                }
+            }
+            else
+            {
+                mStartText.color -= color;
+                if (mStartText.color.a <= 0)
+                {
+                    bAscending = true;
+                }
+            }
+        }
+    }
+}
---
+++ Clicker/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    private Camera mMainCamera;
+    [SerializeField]
+    private GameObject mDummy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mMainCamera = Camera.main;
+    }
+
+    private Ray GenerateRay(Vector3 screenPos)
+    {
+        screenPos.z = mMainCamera.nearClipPlane;
+        Vector3 origin = mMainCamera.ScreenToWorldPoint(screenPos);
+        screenPos.z = mMainCamera.farClipPlane;
+        Vector3 dest = mMainCamera.ScreenToWorldPoint(screenPos);
+
+        return new Ray(origin, dest - origin);
+    }
+
+    private bool CheckTouch(out Vector3 vec)
+    {
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Ray ray = GenerateRay(touch.position);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (gameObject == hit.collider.gameObject)
+                        {
+                            vec = hit.point;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        vec = Vector3.zero;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = GenerateRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(gameObject == hit.collider.gameObject)
+                {
+                    GameObject gameObj = Instantiate(mDummy);
+                    gameObj.transform.position = hit.point;
+                    //TODO Gamecontroller Touch
+                }
+            }
+            //LayerMask aa = LayerMask.NameToLayer("Building") + LayerMask.NameToLayer("Enemy");
+            //LayerMask mask = (1 << LayerMask.NameToLayer("Building")) +
+            //                 (1 << LayerMask.NameToLayer("Enemy"));
+            //int layerMask = 256 + 512;
+            //layerMask = (1 << 8) + (1 << 9);
+            //RaycastHit2D hit2D = Physics2D.Raycast(Vector2.one, Vector2.zero, 0, layerMask);
+        }
+        Vector3 pos;
+        if(CheckTouch(out pos))
+        {
+            // TODO Gamecontroller touch 
+            // set effectPos
+        }
+        
+    }
+}
