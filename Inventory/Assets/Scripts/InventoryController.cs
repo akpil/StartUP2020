dac7a597b5cada7e8c42a2980ba80cf1c991173e
Akpil <jyk0802@naver.com>
2020-06-25 11:56:23 +0900

    Inventory setup

---
+++ Inventory/Assets/Scripts/InventoryController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    private const int SLOT_COUNT = 35;
+    public static InventoryController Instance;
+    private List<ItemData> mItemInfoList;
+
+#pragma warning disable 0649
+    [SerializeField]
+    private ItemController mItemController;
+    [SerializeField]
+    private InventorySlot mSlotPrefab;
+    [SerializeField]
+    private Transform mSlotParents;
+#pragma warning restore 0649
+
+    private InventorySlot[] mSlotArr;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+
+            mItemInfoList = new List<ItemData>();
+            mSlotArr = new InventorySlot[SLOT_COUNT];
+            for(int i = 0; i < SLOT_COUNT; i++)
+            {
+                mSlotArr[i] = Instantiate(mSlotPrefab, mSlotParents);
+                mSlotArr[i].Init(i, null);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddItem(ItemData data)
+    {
+        mItemInfoList.Add(data);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =0; i < 31; i++)
+        {
+            mItemInfoList.Add(mItemController.GetItem(i));
+            mSlotArr[i].SetSprite(mItemController.GetItemSprite(i));
+        }
+    }
+}
---
+++ Inventory/Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour, IDragHandler
+{
+    [SerializeField]
+    private Image mItemImage;
+    private int mID;
+
+    public void Init(int id, Sprite image)
+    {
+        mID = id;
+        mItemImage.sprite = image;
+    }
+
+    public void SetSprite(Sprite image)
+    {
+        mItemImage.sprite = image;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+}
